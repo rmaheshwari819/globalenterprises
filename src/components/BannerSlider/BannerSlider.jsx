88623948d69cf703e9054ef927d9ce79ed003c58
Jignesh <jignesh.sharma.707@gmail.com>
2023-12-28 13:48:04 +0530

    added: responsive done for bannerSlider

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -15,32 +15,38 @@ const BannerSlider = () => {
     {
       image: Slider1,
       heading: 'Civil Construction & Renovation',
-      subheading: 'From ground-up construction to meticulous renovations, we bring expertise, precision, and a keen eye to project.',
+      subheading:
+        'From ground-up construction to meticulous renovations, we bring expertise, precision, and a keen eye to project.',
     },
     {
       image: Slider2,
       heading: 'Fabrication & Mechanical Job',
-      subheading: 'We specialize in precision fabrication, crafting customized structures with cutting-edge technology and secure assembly of heavy equipment',
+      subheading:
+        'We specialize in precision fabrication, crafting customized structures with cutting-edge technology and secure assembly of heavy equipment',
     },
     {
       image: Slider3,
       heading: 'Operation & Maintenance',
-      subheading: 'We excel in seamless operation and maintenance services, ensuring optimal functionality and longevity for your assets',
+      subheading:
+        'We excel in seamless operation and maintenance services, ensuring optimal functionality and longevity for your assets',
     },
     {
       image: Slider4,
       heading: 'Earthmovers & Crane Supply',
-      subheading: 'We provide top-tier Earthmover Services, offering heavy-duty equipment for efficient project execution, deliver reliable lifting solutions',
+      subheading:
+        'We provide top-tier Earthmover Services, offering heavy-duty equipment for efficient project execution, deliver reliable lifting solutions',
     },
     {
       image: Slider5,
       heading: 'Man Power & Electrical Works',
-      subheading: 'From installations to intricate system designs, we deliver top-notch solutions that meet the highest industry standards.',
+      subheading:
+        'From installations to intricate system designs, we deliver top-notch solutions that meet the highest industry standards.',
     },
     {
       image: Slider6,
       heading: 'Structure & Equipment Erection',
-      subheading: 'Our dedicated team is committed to keeping your operations running smoothly and efficiently.',
+      subheading:
+        'Our dedicated team is committed to keeping your operations running smoothly and efficiently.',
     },
   ];
 
@@ -54,9 +60,9 @@ const BannerSlider = () => {
     >
       {BannerData.map((item, index) => (
         <Carousel.Item key={index}>
-          <motion.div className="img-container">
-            <motion.div style={{ width: '100%', height: '100%' }}>
-              <motion.img
+          <div className="img-container">
+            <div style={{ width: '100%', height: '100%' }}>
+              <img
                 initial={{ scale: 1 }}
                 whileInView={{ scale: 1.3 }}
                 transition={{ duration: 12.5, ease: 'easeIn' }}
@@ -64,21 +70,24 @@ const BannerSlider = () => {
                 style={{ width: '100%', height: '100%', contain: 'cover' }}
                 alt={item.heading}
               />
-            </motion.div>
-            <motion.div
-              initial={{ scale: 0, opacity: 0 }}
-              whileInView={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 1.2, delay: 0.6, ease: 'easeInOut' }}
-              className="textbox-design"
-            >
-              <motion.h1>{item.heading}</motion.h1>
-              <motion.h2>{item.subheading}</motion.h2>
-              <Link to="/services">
-                  <button className="mt-5">Read more</button>
-              </Link>
-              {/* <motion.button className="mt-5">Read more</motion.button> */}
-            </motion.div>
-          </motion.div>
+            </div>
+            <div style={{ border: '1px solid red' }}>
+              <div
+                initial={{ scale: 0, opacity: 0 }}
+                whileInView={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1.2, delay: 0.6, ease: 'easeInOut' }}
+                className="textbox-design px-lg-0 px-md-0 px-sm-0 px-2"
+              >
+                <h1>{item.heading}</h1>
+                <h2>{item.subheading}</h2>
+                <Link to="/services">
+                  <button className="mt-lg-5 mt-md-5 mt-sm-4 mt-3">
+                    Read more
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </Carousel.Item>
       ))}
     </Carousel>
